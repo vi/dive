86697b160c5f035c565792fa2928a32d4ca8715e
Vitaly _Vi Shukela <vi0oss@gmail.com>
2013-02-15 06:41:36 +0300

    Fix return code from safer_{read,write} functions

--- safer.c
+++ safer.c
@@ -2,27 +2,33 @@
 #include <errno.h>
 
 int safer_write(int f, char* b, int n) {
+    int acc = 0;
     while(n) {
         int r = write(f, b, n);        
-        if(!r) return 0;
+        if(!r) return acc;
         if(r==-1) {
             if (errno==EINTR || errno==EAGAIN) continue;
             return -1;
         }
         n-=r;
         b+=r;
+        acc+=r;
     }
+    return acc;
 }
 
 int safer_read(int f, char* b, int n) {
+    int acc = 0;
     while(n) {
         int r = read(f, b, n);        
-        if(!r) return 0;
+        if(!r) return acc;
         if(r==-1) {
             if (errno==EINTR || errno==EAGAIN) continue;
             return -1;
         }
         n-=r;
         b+=r;
+        acc+=r;
     }
+    return acc;
 }
